ce22809cb77572c0094151b7d7b257e9135f4491
Samuel Overs Pelham <pelham4lib.2018@gmail.com>
2021-11-25 20:24:33 +0800

    initial development
    First commit

---
+++ src/essohpee/Main.java
@@ -0,0 +1,36 @@
+package essohpee;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Main extends Application {
+
+    private static Stage stg;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        stg = primaryStage;
+        primaryStage.setResizable(false);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
+        primaryStage.setTitle("Typing Speed");
+        primaryStage.getIcons().add(new Image("images/keyboard.png"));
+        primaryStage.setScene(new Scene(root, 800, 450));
+        primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxml)));
+        stg.getScene().setRoot(pane);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
